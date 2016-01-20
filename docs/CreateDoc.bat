d64bbd6792695247a14fc998e48eef6be964dffb
hissa <hissamassa7@gmail.com>
2016-01-20 22:43:45 +0900

    CarbonとPhpDocumentorのファイルを設置。

---
+++ docs/CreateDoc.bat
@@ -0,0 +1,4 @@
+echo on
+cd /d D:\Programfiles\Apache\Apache24\htdocs\Timetable3
+phpdoc -d D:\Programfiles\Apache\Apache24\htdocs\Timetable3\ -t D:\Programfiles\Apache\Apache24\htdocs\Timetable3\docs -o HTML:Smarty:PHP
+pause
