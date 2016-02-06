45f74ece4abcce212f8e1e89c162a4ff75247e27
hissa <hissamassa7@gmail.com>
2016-02-06 15:08:47 +0900

    最初にtimetable1を表示するページを作成。

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <?php include "./common.php"; ?>
+    </head>
+    <body>
+        <?php include "./parts/header.html"; ?>
+        <div class="container">
+            <?php include "./parts/timetable1.php"; ?>
+        </div>
+    </body>
+</html>
---
+++ parts/timetable1.php
@@ -0,0 +1,35 @@
+<?php
+$SETTINGS = new Config("./config.ini", "./timetable1.ini");
+$table = new Timetable();
+?>
+<ul class="nav nav-tabs">
+    <?php
+    $start = explode(",", $SETTINGS->showPeriodWeek)[0];
+    $end = explode(",", $SETTINGS->showPeriodWeek)[1];
+    for($i = $start; $i <= $end; $i++){
+        $date = null;
+        $date = TimetableCarbon::now()->addWeeks($i);
+        if($i == 0){
+            echo "<li class=\"active\"><a href=\"#table".$i."\" data-toggle=\"tab\">";
+            echo "今週";
+        }else{
+            echo "<li><a href=\"#table".$i."\" data-toggle=\"tab\">";
+            echo $date->format("n月j日～");
+        }
+        echo "</a></li>";
+    }
+    ?>
+</ul>
+<div class="tab-content">
+    <?php
+    for($i = $start; $i <= $end; $i++){
+        if($i == 0){
+            echo "<div class=\"tab-pane active\" id=\"table".$i."\">";
+        }else{
+            echo "<div class=\"tab-pane\" id=\"table".$i."\">";
+        }
+        echo $table->create($i);
+        echo "</div>";
+    }
+    ?>
+</div>
