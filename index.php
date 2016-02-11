c7bd03a50a86a2087a2a0c64a7b3752b7bc329ad
hissa <hissamassa7@gmail.com>
2016-02-11 15:39:10 +0900

    WEBページとしての表示を整理
    トップページ、エラーページを追加

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
     <body>
         <?php include "./parts/header.html"; ?>
         <div class="container">
-            <?php include "./parts/timetable1.php"; ?>
+            <?php
+            switch($_GET["page"]){
+                case null:
+                    include "./parts/toppage.php";
+                    break;
+                case "timetable1":
+                    include "./parts/timetable1.php";
+                    break;
+                default:
+                    include "./parts/error.php";
+                    break;
+            }
+            ?>
         </div>
     </body>
 </html>
---
+++ parts/error.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger" role="alert">
+    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+    ページが見つかりませんでした。
+</div>
