c7e77b89fbffe095b046efd4d19c43070e1f3448
hissa <hissamassa7@gmail.com>
2016-02-13 15:44:55 +0900

    管理画面と管理者を扱うクラスのファイルを追加。

---
+++ admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <?php include "./common.php"; ?>
+        <?php TimetableCarbon::setTestNow(TimetableCarbon::parse("2016/2/8")) ?>
+    </head>
+    <body>
+        <?php include "./parts/header.html"; ?>
+        <div class="container">
+            <?php
+            switch($_GET["page"]){
+                case null:
+                    include "./parts/toppage.php";
+                    break;
+                case "timetable1":
+                    include "./parts/timetable1.php";
+                    break;
+                default:
+                    include "./parts/error.php";
+                    break;
+            }
+            ?>
+        </div>
+    </body>
+</html>
